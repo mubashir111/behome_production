891e2b632d4f4cff98f5d1f7f4256d0a6f35a89e
mubashir111 <muba4shir@gmail.com>
2026-04-02 15:46:07 +0530

    changed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
         $this->call(CompanyTableSeeder::class);
         $this->call(ThemeTableSeeder::class);
         $this->call(CookiesTableSeeder::class);
