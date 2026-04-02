1f1c49a065f7a94680a4de9d18adfef6af7ca776
mubashir111 <muba4shir@gmail.com>
2026-04-02 15:22:29 +0530

    changed the cookiestableseeder

--- database/seeders/CookiesTableSeeder.php
+++ database/seeders/CookiesTableSeeder.php
@@ -2,10 +2,8 @@
 
 namespace Database\Seeders;
 
-
-use Dipokhalder\EnvEditor\EnvEditor;
-use Smartisan\Settings\Facades\Settings;
 use Illuminate\Database\Seeder;
+use Smartisan\Settings\Facades\Settings;
 
 
 class CookiesTableSeeder extends Seeder
@@ -17,12 +15,10 @@ class CookiesTableSeeder extends Seeder
      */
     public function run()
     {
-        $envService = new EnvEditor();
+        $isDemo = env('DEMO', false);
         Settings::group('cookies')->set([
-            'cookies_details_page_id' => $envService->getValue('DEMO') ? 5 : 0,
-            'cookies_summary'         => $envService->getValue(
-                'DEMO'
-            ) ? 'This website uses cookies to better understand how visitors use our site, for advertising, and to offer you a more personalized experience. We share information about your use of our site with analytics, social media, and advertising partners in according with our Privacy Statement. You can manage this sharing by selecting the "Cookie Settings" link.' : ''
+            'cookies_details_page_id' => $isDemo ? 5 : 0,
+            'cookies_summary'         => $isDemo ? 'This website uses cookies to better understand how visitors use our site, for advertising, and to offer you a more personalized experience. We share information about your use of our site with analytics, social media, and advertising partners in according with our Privacy Statement. You can manage this sharing by selecting the "Cookie Settings" link.' : ''
         ]);
     }
 }
