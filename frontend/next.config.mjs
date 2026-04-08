816fb0da169f00f092687da984de15a4a8034466
mubashir111 <muba4shir@gmail.com>
2026-04-08 22:06:20 +0530

    updated

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -26,6 +26,8 @@ const nextConfig = {
       { protocol: 'https', hostname: 'localhost' },
       { protocol: 'http',  hostname: '127.0.0.1' },
       { protocol: 'https', hostname: '127.0.0.1' },
+      { protocol: 'https', hostname: 'behom.ae' },
+      { protocol: 'https', hostname: 'www.behom.ae' },
       { protocol: 'https', hostname: '*.digitalocean.com' },
     ],
   },
